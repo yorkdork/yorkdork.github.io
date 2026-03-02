4dcfc929b1ea9693bd78ae5bd54f1e1d98002340
yorkdork <140773882+yorkdork@users.noreply.github.com>
2026-03-02 04:29:31 +0000

    Update test

--- website/src/app/app.spec.ts
+++ website/src/app/app.spec.ts
@@ -18,6 +18,7 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, website');
+    expect(compiled.querySelector('h1')?.textContent).toContain('York');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Raccoon');
   });
 });
